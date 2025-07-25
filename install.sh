40aaaea60f14e2493d047ca257a4ab439b137891
RazorSec <164746457+RazorSettings@users.noreply.github.com>
2025-07-25 15:07:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
+cat > install.sh << EOF
 #!/data/data/com.termux/files/usr/bin/bash
 
 # ================= AUTO RUN INSTALL REALSENSI =================
 
 spinner() {
-    local pid=$!
+    local pid=\$!
     local delay=0.1
-    local spinstr='|/-\'
+    local spinstr='|/-\\'
     printf "Installing dependencies... "
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
+    while [ "\$(ps a | awk '{print \$1}' | grep \$pid)" ]; do
+        local temp=\${spinstr#?}
+        printf " [%c]  " "\$spinstr"
+        local spinstr=\$temp\${spinstr%"\$temp"}
+        sleep \$delay
         printf "\b\b\b\b\b\b"
     done
     printf "    \b\b\b\b"
@@ -32,3 +33,7 @@ install_modules() {
 }
 
 install_modules
+EOF
+
+chmod +x install.sh
+./install.sh
